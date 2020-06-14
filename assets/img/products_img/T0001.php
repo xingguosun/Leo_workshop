5d9c21bf5909c060a4bfcf106dcbf6f8a87d29e5
s181385 <s181385@student.dtu.dk>
2020-06-15 00:32:25 +0200

    first

---
+++ assets/img/products_img/T0001.php
@@ -0,0 +1,168 @@
+<HTML><HEAD>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" >
+<TITLE>Leoworkshop:Maak je eigen T-shirt !</title>
+<style type="text/css">
+    body {
+	margin:0;
+	padding:0;
+	font: bold 11px/1.5em Verdana;
+}
+
+h1 {
+	font: bold 12px Verdana, Arial, Helvetica, sans-serif;
+	color: #000;
+	margin: 0px;
+	padding: 0px 0px 0px 15px;
+}
+h2 {
+	font: bold 14px Verdana, Arial, Helvetica, sans-serif;
+	color: #000;
+	margin: 0px;
+	padding: 0px 0px 0px 15px;
+}
+ 
+    #tabs {
+      float:left;
+      width:100%;
+      background:#EFF4FA;
+      font-size:12px;
+      line-height:normal;
+	  border-bottom:1px solid #DD740B;
+      }
+    #tabs ul {
+	margin:0;
+	padding:10px 10px 0 50px;
+	list-style:none;
+      }
+    #tabs li {
+      display:inline;
+      margin:0;
+      padding:0;
+      }
+    #tabs a {
+      float:left;
+      background:url('leoworkshop/tableftI.gif') no-repeat left top;
+      margin:0;
+      text-decoration:none; padding-left:5px; padding-right:0; padding-top:0; padding-bottom:0
+      }
+    #tabs a span {
+      float:left;
+      display:block;
+      background:url('leoworkshop/tabrightI.gif') no-repeat right top;
+      color:#FFF; padding-left:6px; padding-right:15px; padding-top:5px; padding-bottom:4px
+      }
+    #tabs a span {float:none;}
+    #tabs a:hover span {
+      color:#FFF;
+      }
+    #tabs a:hover {
+      background-position:0% -42px;
+      }
+    #tabs a:hover span {
+      background-position:100% -42px;
+      }
+</style>
+</head>
+
+<body>
+
+<div align="center">
+  <center>
+<table cellSpacing="0" cellPadding="0" border="0" style="border-collapse: collapse" bordercolor="#111111" width="900">
+  <tr>
+    <td height="18" align="center" width="900">
+    <table border="1" cellpadding="0" cellspacing="0" style="border-collapse: collapse" bordercolor="#C0C0C0" width="100%" id="AutoNumber17" >
+      <tr>
+        <td width="100%" bgcolor="#6699FF" height="35">
+        <font style="font-weight:700" color="#FFFFFF" >
+        &nbsp;Product Information</font></td>
+      </tr>
+      <tr>
+        <td >
+  <?
+    $artnr = $_GET['artnr'];
+    $gif =".gif" ;
+    $col = $_GET['col'];
+ ?>   
+<img border="0" src="../model/<?= $artnr?>A<?=$gif?> " width="420" height="380" >
+      </td></tr>
+      <tr>
+        <td width="100%" height="124">Art nr.:<?=$artnr?><p>Men/Unisex T-Shirt <?=$col?></p>
+        <ul>
+          <li>100% Cotton </li>
+          <li>155 GSM</li>
+        </ul>
+        </td>
+      </tr>
+      <tr>
+        <td align="center" height="188">
+        <p align="left"><font size="2">Details:(attention! can make 5% derogate)</font></p>
+        <table border="1" cellpadding="0" cellspacing="0" style="border-collapse: collapse" bordercolor="#C0C0C0" width="99%" id="AutoNumber18">
+          <tr>
+            <td width="20%" rowspan="4"><font size="2">
+            <img border="0" src="../leoworkshop/t-shirt_info.jpg" width="135" height="114"></font></td>
+            <td width="24%"><font size="2">&gt; Sizes</font></td>
+            <td align="center"><font size="2">S</font></td>
+            <td align="center"><font size="2">M</font></td>
+            <td align="center"><font size="2">L</font></td>
+            <td align="center"><font size="2">XL</font></td>
+            <td align="center"><font size="2">XXL</font></td>
+            <td align="center"><font size="2">XXXL</font></td>
+            <td align="center"><font size="2">4XL</font></td>
+          </tr>
+          <tr>
+            <td width="24%"><font size="2">A HALF CHEST(cm)</font></td>
+            <td align="center"><font size="2">50</font></td>
+            <td align="center"><font size="2">53</font></td>
+            <td align="center"><font size="2">56</font></td>
+            <td align="center"><font size="2">59</font></td>
+            <td align="center"><font size="2">62</font></td>
+	    <td align="center"><font size="2">64</font></td>
+            <td align="center"><font size="2">67</font></td>
+
+          </tr>
+          <tr>
+            <td width="24%"><font size="2">B BODY LENGTH(cm)</font></td>
+            <td align="center"><font size="2">69</font></td>
+            <td align="center"><font size="2">72</font></td>
+            <td align="center"><font size="2">74</font></td>
+            <td align="center"><font size="2">76</font></td>
+            <td align="center"><font size="2">78</font></td>
+            <td align="center"><font size="2">83</font></td>
+            <td align="center"><font size="2">85</font></td>
+          </tr>
+          <tr>
+            <td width="24%"><font size="2">C SLEEVE LEGTH(cm)</font></td>
+            <td align="center"><font size="2">20</font></td>
+            <td align="center"><font size="2">20</font></td>
+            <td align="center"><font size="2">20</font></td>
+            <td align="center"><font size="2">21</font></td>
+            <td align="center"><font size="2">21</font></td>
+            <td align="center"><font size="2">23</font></td>
+            <td align="center"><font size="2">23</font></td>
+     
+          </tr>
+        </table>
+        <p>&nbsp;</td>
+      </tr>
+      <tr>
+        <td align="center" >
+     <form> 
+<input type=button value="close window" onClick="javascript:window.close();">
+</td></form>
+ </tr>
+    </table>
+    </td>
+  </tr>
+  <tr>
+    <td height="18" width="961"></td>
+  </tr><tr>
+   <td width="100%" bgcolor="#808080" height="16">
+    <p align="center">
+    <font style="FONT-SIZE: 10px" face="Verdana" color="#ffffff" size="-1">
+    © 2007 by: Leo Tech</font>
+  </td></tr>
+</table>
+</body>
+
+</html>
